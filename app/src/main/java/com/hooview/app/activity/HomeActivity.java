83893cfd393afb21853f7932ea930b514665fcad
yinyongliang <1060745597@qq.com>
2016-11-11 16:08:48 +0800

    首页UI初步

--- app/src/main/java/com/hooview/app/activity/HomeActivity.java
+++ app/src/main/java/com/hooview/app/activity/HomeActivity.java
@@ -1,15 +1,37 @@
 package com.hooview.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
 
 import com.hooview.app.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.mViewPager)
+    ViewPager mViewPager;
+    @BindView(R.id.radio_home)
+    RadioButton mRadioHome;
+    @BindView(R.id.radio_consult)
+    RadioButton mRadioConsult;
+    @BindView(R.id.radio_me)
+    RadioButton mRadioMine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+//        mRadioHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(HomeActivity.this, "home", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
