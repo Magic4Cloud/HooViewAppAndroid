ad1a55e6b042a1890333fda668c11cfe5fa6f6b3
yinyongliang <1060745597@qq.com>
2016-11-21 13:09:58 +0800

    引用gradle依赖升级连麦功能

--- app/src/main/java/com/hooview/app/activity/live/LivePrepareActivity.java
+++ app/src/main/java/com/hooview/app/activity/live/LivePrepareActivity.java
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.hooview.app.R;
+import com.hooview.app.base.BaseActivity;
 
 /**
  * 直播准备页面
  * 功能：获取封面，获取
  */
-public class LivePrepareActivity extends AppCompatActivity {
+public class LivePrepareActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
